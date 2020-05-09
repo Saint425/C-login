476263b1d62b97e3118e0e40f0cd8807525d0997
Nick <JBlythe425@gmail.com>
2020-05-09 10:33:49 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace time_date
+{
+    class Time
+    {
+        static void Main()
+        {
+            string user;
+            Console.WriteLine("User Name: ");
+            user = Console.ReadLine();
+            Console.WriteLine("Hello, {0}", user);
+            DateTime localDate = DateTime.Now;
+            DateTime utcDate = DateTime.UtcNow;
+            Console.WriteLine("Current Time is {0}", localDate.ToString()); 
+            Console.WriteLine("Current UTC Time is {0}", utcDate.ToString());
+        }
+}
+}
